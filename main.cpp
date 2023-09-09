ea4c571174fbb53c99602c3d21892625c2a53964
PouyaGohari <pouya.hmgohari@gmail.com>
2023-09-09 18:30:48 +0330

    Add main.cpp and headers

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char*argv[]){
+    
+    return 0;
+}
